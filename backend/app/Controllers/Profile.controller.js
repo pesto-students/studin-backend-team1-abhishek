14166e80de5206b69a368a8e1c4ab39a0701ed5c
vishaljsdeveloper <105301814+vishaljsdeveloper@users.noreply.github.com>
2022-10-22 16:41:41 +0530

    backend api for edit profile

--- backend/app/Controllers/Profile.controller.js
+++ backend/app/Controllers/Profile.controller.js
@@ -21,15 +21,49 @@ const getMyProfileSummary = async (req, res) => {
 
 const getMyProfileDetails = async (req, res) => {
   try {
-    return;
+    res.json(req.user);
   } catch (error) {
-    return;
+   console.log(error)
   }
 };
 
 const updateMyProfileDetails = async (req, res) => {
   try {
-    return;
+    if (!req.body) {
+      res.status(400).send('Insufficient data');
+  }
+
+  const {
+      userId, firstName, lastName, schoolName, collegeName, interests,
+  } = req.body;
+  const { profilePhoto, coverPhoto } = req.files;
+  console.log(req.user)
+
+  const imageResult = await cloudinary.uploader.upload(profilePhoto.tempFilePath, {
+      public_id: `${Date.now()}`,
+      resource_type: 'auto',
+      folder: 'studin/users-profile-images',
+  });
+
+  const imageResult2 = await cloudinary.uploader.upload(coverPhoto.tempFilePath, {
+      public_id: `${Date.now()}`,
+      resource_type: 'auto',
+      folder: 'studin/users-cover-images',
+  });
+
+  const payload = {
+      email: userId,
+      firstName: firstName,
+      lastName: lastName,
+      schoolName: schoolName,
+      collegeName: collegeName,
+      interests: interests,
+      profilePhoto: imageResult.secure_url,
+      coverPhoto: imageResult2.secure_url,
+  };
+  const updateuser = await User.findByIdAndUpdate(req.user._id, payload, {
+      new: true
+  });
   } catch (error) {
     return;
   }
