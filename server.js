6d39b6129b98e20d291c2c8e9c13f931e729e0d9
Jagannathan J <jjagannn@gmail.com>
2022-10-29 15:00:15 +0530

    Updated PORT ref

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, '0.0.0.0', ()=>{
-  console.log(`StudIn API is listening on port ${port}`);
+  console.log(`StudIn API is listening on port ${PORT}`);
 });
