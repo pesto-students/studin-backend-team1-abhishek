96aaa6d00f1e683a48e4e2c5e4df660b5d26a178
Jagannathan J <jjagannn@gmail.com>
2022-10-29 13:49:59 +0530

    Updated build

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.port || 3001;
+const port = process.env.port || 8080;
 
 app.listen(port, '0.0.0.0', ()=>{
   console.log(`StudIn API is listening on port ${port}`);
